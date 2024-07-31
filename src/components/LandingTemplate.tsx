849f07d6be9b97eafa7d1c4f0f9b1543906fe9c9
FadhilKholaf <fadhilgd123@gmail.com>
2024-07-31 08:48:44 +0700

    fix: responsive ui bug

--- src/components/LandingTemplate.tsx
+++ src/components/LandingTemplate.tsx
@@ -9,7 +9,7 @@ export default function LandingTemplate({
 }) {
   return (
     <section className="flex h-screen w-screen flex-col items-center justify-between bg-purple px-4 pt-24 text-white lg:flex-row lg:px-20 lg:pt-0">
-      <div className="flex w-full flex-col gap-y-8 lg:w-1/3">
+      <div className="z-10 flex w-full flex-col gap-y-8 lg:w-1/3">
         <h1 className="text-4xl font-bold lg:text-6xl">{title}</h1>
         <p className="poppins text-justify lg:text-xl">{description}</p>
       </div>
